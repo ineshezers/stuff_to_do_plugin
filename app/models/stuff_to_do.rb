7b5ccfaf29ed1ba1888f91d27de3ce9fabbb0310
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-10-22 17:54:44 +0400

    Display only project with at least one issue

--- app/models/stuff_to_do.rb
+++ app/models/stuff_to_do.rb
@@ -247,7 +247,8 @@ class StuffToDo < ActiveRecord::Base
   end
 
   def self.active_and_visible_projects(user=User.current)
-    projects = Project.active.where(Project.visible_condition(user))
+    projects = self.available(user, nil, user).collect{ |issue| issue.project }.uniq
+#    projects = Project.active.where(Project.visible_condition(user))
     if !User.current.allowed_to_globally?(:view_all_reportee_issues, {}) and (user != User.current)
       projects = projects.where(Project.visible_condition(User.current))
     end
